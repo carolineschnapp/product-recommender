2e1691c1eac876905018e8fff0623fae6e0ca969
Caroline Hill <mllegeorgesand@gmail.com>
2013-09-19 11:40:44 -0400

    Repair major fuckup that happended with a find and replace

--- product-recommender.js
+++ product-recommender.js
@@ -1,12 +1,12 @@
 /* Sample JavaScript file added with ScriptTag resource. 
 This sample file is meant to teach best practices.
-Your app will load $ if it's not defined. 
-Your app will load $ if $ is defined but is too old, e.g. < 1.7. 
-Your app does not change the definition of $ or $ outside the app. 
-Example: if a Shopify theme uses $ 1.4.2, both of these statements run in the console will still return '1.4.2'
-once the app is installed, even if the app uses $ 1.9.1:
-$.fn.$ => "1.4.2" 
-$.fn.$ -> "1.4.2"
+Your app will load jQuery if it's not defined. 
+Your app will load jQuery if jQuery is defined but is too old, e.g. < 1.7. 
+Your app does not change the definition of $ or jQuery outside the app. 
+Example: if a Shopify theme uses jQuery 1.4.2, both of these statements run in the console will still return '1.4.2'
+once the app is installed, even if the app uses jQuery 1.9.1:
+jQuery.fn.jquery => "1.4.2" 
+$.fn.jquery -> "1.4.2"
 */
 
 /* Using a self-executing anonymous function - (function(){})(); - so that all variables and functions defined within 
@@ -14,7 +14,7 @@ aren’t available to the outside world. */
 
 (function(){
   
-/* Load Script function we may need to load $ from the Google's CDN */
+/* Load Script function we may need to load jQuery from the Google's CDN */
 /* That code is world-reknown. */
 /* One source: http://snipplr.com/view/18756/loadscript/ */
 
@@ -61,17 +61,17 @@ var myAppJavaScript = function($){
   $('.hide-if-no-app-js').show();
 };
 
-/* If $ has not yet been loaded or if it has but it's too old for our needs,
-we will load $ from the Google CDN, and when it's fully loaded, we will run
+/* If jQuery has not yet been loaded or if it has but it's too old for our needs,
+we will load jQuery from the Google CDN, and when it's fully loaded, we will run
 our app's JavaScript. Set your own limits here, the sample's code below uses 1.7
-as the minimum version we are ready to use, and if the $ is older, we load 1.9. */
-if ((typeof $ === 'undefined') || (parseFloat($.fn.$) < 1.7)) {
-  loadScript('//ajax.googleapis.com/ajax/libs/$/1.9.1/$.min.js', function(){
-    $191 = $.noConflict(true);
-    myAppJavaScript($191);
+as the minimum version we are ready to use, and if the jQuery is older, we load 1.9. */
+if ((typeof jQuery === 'undefined') || (parseFloat(jQuery.fn.jquery) < 1.7)) {
+  loadScript('//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js', function(){
+    jQuery191 = jQuery.noConflict(true);
+    myAppJavaScript(jQuery191);
   });
 } else {
-  myAppJavaScript($);
+  myAppJavaScript(jQuery);
 }
 
 })();
